4bed8da37c394c8e248cf1420b08a4a7d8b1cf2f
smatts <smatts@posteo.de>
2023-08-24 17:17:29 +0200

    Create a super basic landing page
    Also, for that, index.md slides can no longer exist!

--- .github/workflows/create_slides_helper.sh
+++ .github/workflows/create_slides_helper.sh
@@ -7,6 +7,18 @@ do
     rm -f $file
 done
 
+# If a file is called index, rename the file
+md_files=$(find slides/*.md -exec basename \{} .md \;)
+for file in $md_files
+do
+    if [ $file == "index" ]; then
+        echo "$file is a forbidden name for a slide."
+        echo "Renaming $file.md to index-slide.md"
+        mv slides/$file.md slides/index-slide.md
+        ls
+    fi
+done
+
 # Create new HTML and PDF files
 cd config
 npx @marp-team/marp-cli@latest
@@ -14,3 +26,14 @@ npx @marp-team/marp-cli@latest --pdf
 cd ..
 mkdir -p pdf
 cp public/*.pdf pdf/
+
+# Create index.html
+touch index.md
+echo "# Slides\n" > index.md
+pdf_files=$(find pdf/*.pdf -exec basename \{} .pdf \;)
+for file in $pdf_files
+do
+    echo $file
+    echo "* ${file%.pdf}: [HTML]($file.html) / [PDF](pdf/$file.pdf)" >> index.md
+done
+pandoc index.md -o index.html
